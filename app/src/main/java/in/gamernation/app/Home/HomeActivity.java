b4d3401de64cf9c5b97cbe99f141ae77e6c7f822
Pk <talk2prajjwal@gmail.com>
2021-07-06 19:47:45 +0530

    first commit

---
+++ app/src/main/java/in/gamernation/app/Home/HomeActivity.java
@@ -0,0 +1,16 @@
+package in.gamernation.app.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.gamernation.app.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/in/gamernation/app/Startup/Splash.java
@@ -0,0 +1,16 @@
+package in.gamernation.app.Startup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.gamernation.app.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
